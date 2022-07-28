8d86e1eab9fd67671005f811ba855c2934f26fde
Marvin Deckmyn <marvindeckmyn@gmail.com>
2022-07-28 23:20:05 +0200

    Add account model

---
+++ model/account/account.go
@@ -0,0 +1,10 @@
+package account
+
+import "github.com/marvindeckmyn/drankspelletjes-server/uuid"
+
+type Account struct {
+	ID       *uuid.UUID `json:"id"`
+	Name     *string    `json:"name"`
+	Email    *string    `json:"email"`
+	Password *string    `json:"password"`
+}
